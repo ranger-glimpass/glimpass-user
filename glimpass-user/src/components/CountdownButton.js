b6b9be9730a6ab67f6b7797075269729617709c9
Manish bharti <parasparas0001@gmail.com>
2023-10-29 13:23:59 +0530

    v3 changes..

---
+++ glimpass-user/src/components/CountdownButton.js
@@ -0,0 +1,40 @@
+// src/CountdownButton.js
+import React, { useState, useEffect } from "react";
+import Button from "@mui/material/Button";
+
+const CountdownButton = ({ handlePrevious, buttonText }) => {
+  const [count, setCount] = useState(3);
+
+  useEffect(() => {
+    setCount(3);
+  }, [buttonText]);
+
+  useEffect(() => {
+    let timer;
+    if (count > 0) {
+      timer = setInterval(() => {
+        if (count === 1) {
+          clearInterval(timer);
+        }
+        setCount((prevCount) => prevCount - 1);
+      }, 1000);
+    }
+    return () => {
+      clearInterval(timer);
+    };
+  }, [count]);
+
+  return (
+    <div>
+      <Button
+        variant="outlined"
+        color="primary"
+        onClick={count > 0 ? null : handlePrevious}
+      >
+        {count === 0 ? `${buttonText}` : `Counting down: ${count}`}
+      </Button>
+    </div>
+  );
+};
+
+export default CountdownButton;
--- glimpass-user/src/components/Dashboard.js
+++ glimpass-user/src/components/Dashboard.js
@@ -12,12 +12,12 @@ import {
   Container,
   TextField,
 } from "@mui/material";
-import { Carousel } from 'react-responsive-carousel';
+import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // Import carousel styles
 
-import facingToShop from '../assets/facingToShop.gif';
-import goingToShop from '../assets/goingToShop.gif';
-
+import facingToShop from "../assets/facingToShop.gif";
+import goingToShop from "../assets/goingToShop.gif";
+import CountdownButton from "./CountdownButton";
 
 const Dashboard = () => {
   const location = useLocation();
@@ -33,33 +33,35 @@ const Dashboard = () => {
   const handleMotion = () => {};
   const handleOrientation = () => {};
 
-
   const [currentSlide, setCurrentSlide] = useState(0);
 
-    const handleNext = () => {
-        if (currentSlide === 0) {
-            setCurrentSlide(1);
-        } else {
-            requestPermission();
-        }
-    };
+  const handleNext = () => {
+    if (currentSlide === 0) {
+      setCurrentSlide(1);
+    } else {
+      requestPermission();
+    }
+  };
 
-    const handlePrevious = () => {
-        setCurrentSlide(0);
-    };
+  const handlePrevious = () => {
+    setCurrentSlide(0);
+  };
 
   useEffect(() => {
     const fetchShops = async () => {
-       const response = await fetch("https://app.glimpass.com/graph/get-all-nodes-by-market", {
+      const response = await fetch(
+        "https://app.glimpass.com/graph/get-all-nodes-by-market",
+        {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
           },
           body: JSON.stringify({
             market: location.state?.market,
-          })
+          }),
           // Add any necessary body data for the POST request
-        });
+        }
+      );
       const data = await response.json();
       const shopsArray = Object.values(data);
       setShops(shopsArray);
@@ -69,22 +71,25 @@ const Dashboard = () => {
     fetchShops();
   }, []);
 
-
-  const [updatedDestinationShopId, setUpdatedDestinationShopId] = useState(destinationShopId);
+  const [updatedDestinationShopId, setUpdatedDestinationShopId] =
+    useState(destinationShopId);
 
   useEffect(() => {
     const fetchUpdatedDestination = async () => {
       if (destinationShopId === "nearestWashroom") {
-        const response = await fetch("https://app.glimpass.com/user/get-nearest-washroom", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            nodeId: currentLocation?.nodeId,
-          })
-          // Add any necessary body data for the POST request
-        });
+        const response = await fetch(
+          "https://app.glimpass.com/user/get-nearest-washroom",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              nodeId: currentLocation?.nodeId,
+            }),
+            // Add any necessary body data for the POST request
+          }
+        );
         const data = await response.json();
         setUpdatedDestinationShopId(data._id);
         //destinationShopId = updatedDestinationShopId;
@@ -93,10 +98,9 @@ const Dashboard = () => {
         console.log("DestinationShopID from different API:", destinationShopId);
       }
     };
-  
+
     fetchUpdatedDestination();
   }, [destinationShopId, currentLocation]);
-  
 
   const requestPermission = () => {
     if (
@@ -143,146 +147,155 @@ const Dashboard = () => {
 
   return (
     <>
-    <Typography
-            variant="h6" // This makes the text bold
-            sx={{ 
-                display: { xs: 'contents', sm: 'block' }, // Hide on smaller screens
-                whiteSpace: 'nowrap', // Prevent wrapping to the next line
-                overflow: 'hidden',
-                textOverflow: 'ellipsis', // Add ellipsis for long names
-                maxWidth: '20%', // Adjust this width as needed
-                pl: '10px',
-                fontSize: '1rem',
-                marginRight: '2px'
-            }}
-        >
-            Hello, {sessionStorage.getItem('name')}!
-        </Typography>
-    <Container>
-      <Box mt={4} display="flex" flexDirection="column" alignItems="center">
-        <Typography variant="h5" gutterBottom>
-          Where are you at?
-        </Typography>
+      <Typography
+        variant="h6" // This makes the text bold
+        sx={{
+          display: { xs: "contents", sm: "block" }, // Hide on smaller screens
+          whiteSpace: "nowrap", // Prevent wrapping to the next line
+          overflow: "hidden",
+          textOverflow: "ellipsis", // Add ellipsis for long names
+          maxWidth: "20%", // Adjust this width as needed
+          pl: "10px",
+          fontSize: "1rem",
+          marginRight: "2px",
+        }}
+      >
+        Hello, {sessionStorage.getItem("name")}!
+      </Typography>
+      <Container>
+        <Box mt={4} display="flex" flexDirection="column" alignItems="center">
+          <Typography variant="h5" gutterBottom>
+            Where are you at?
+          </Typography>
 
-        <Box mt={2} width="100%">
-        <Autocomplete
-    fullWidth
-    options={shops}
-    getOptionLabel={(option) => option.name}
-    value={currentLocation?.nodeId}
-    onChange={(event, newValue) => setCurrentLocation(newValue)}
-    renderInput={(params) => (
-        <TextField {...params} label="Select a Shop" variant="outlined" />
-    )}
-    renderOption={(props, option) => (
-        <Box 
-            component="li" 
-            sx={{ 
-                position: 'relative', 
-                display: 'flex', 
-                justifyContent: 'space-between', 
-                alignItems: 'center', 
-                '& > img': { mr: 2, flexShrink: 0 } 
-            }} 
-            {...props}
-        >
-            {option.name}
-            <Typography 
-                variant="body2" 
-                sx={{ 
-                    position: 'absolute', 
-                    bottom: 0, 
-                    right: 0, 
-                    fontStyle: 'italic', 
-                    fontWeight: 'bold', 
-                    fontSize: '0.6rem' 
-                }}
-            >
-                ({option.floor} floor)
-            </Typography>
-        </Box>
-    )}
-    sx={{
-        '& .MuiAutocomplete-input': {
-            fontSize: '1.2rem', // Increase font size of input
-        },
-        '& .MuiAutocomplete-option': {
-            fontSize: '1.2rem', // Increase font size of dropdown options
-            padding: '10px 15px', // Add padding for elegance
-        },
-        '& .MuiOutlinedInput-root': {
-            borderRadius: '25px', // Rounded corners for elegance
-        },
-    }}
-/>
-
-
-        </Box>
-
-        <Box mt={2}>
-          <Button
-            variant="contained"
-            color="primary"
-            onClick={() => setOpen(true)}
-          >
-            I'm here!
-          </Button>
-        </Box>
-
-       
-
-        <Modal open={open} onClose={() => setOpen(false)}>
-            <Box
-                sx={{
-                    position: "absolute",
-                    top: "50%",
-                    left: "50%",
-                    transform: "translate(-50%, -50%)",
-                    width: "95%",
-                    bgcolor: "background.paper",
-                    border: "2px solid #000",
-                    boxShadow: 24,
-                    p: 4,
-                }}
-            >
-                <Typography variant="h6" id="modal-title">
-                    Calibration Required
-                </Typography>
-                <Carousel
-                    showThumbs={false}
-                    showStatus={false}
-                    showIndicators={false}
-                    selectedItem={currentSlide}
-                    showArrows={false}
+          <Box mt={2} width="100%">
+            <Autocomplete
+              fullWidth
+              options={shops}
+              getOptionLabel={(option) => option.name}
+              value={currentLocation?.nodeId}
+              onChange={(event, newValue) => setCurrentLocation(newValue)}
+              renderInput={(params) => (
+                <TextField
+                  {...params}
+                  label="Select a Shop"
+                  variant="outlined"
+                />
+              )}
+              renderOption={(props, option) => (
+                <Box
+                  component="li"
+                  sx={{
+                    position: "relative",
+                    display: "flex",
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                    "& > img": { mr: 2, flexShrink: 0 },
+                  }}
+                  {...props}
                 >
-                    <div>
-                        <img src={goingToShop} alt="Step 1" />
-                        <Typography variant="body2" id="modal-description" gutterBottom>
-                            <b>Step 1:</b>  Go to the {currentLocation?.name}.
-                        </Typography>
-                    </div>
-                    <div>
-                        <img src={facingToShop} alt="Step 2" />
-                        <Typography variant="body2" id="modal-description" gutterBottom>
-                            <b>Step 2:</b>  Face towards the shop: {currentLocation?.name}.
-                        </Typography>
-                    </div>
-                </Carousel>
-                <Box display="flex" justifyContent="space-between" mt={2}>
-                    {currentSlide === 1 && (
-                        <Button variant="text" onClick={handlePrevious}>
-                            &lt; Previous
-                        </Button>
-                    )}
-                    <Button variant="outlined" onClick={handleNext} style={{ marginTop: '20px' }}>
-                        {currentSlide === 0 ? 'Next' : 'Calibrate'}
-                    </Button>
+                  {option.name}
+                  <Typography
+                    variant="body2"
+                    sx={{
+                      position: "absolute",
+                      bottom: 0,
+                      right: 0,
+                      fontStyle: "italic",
+                      fontWeight: "bold",
+                      fontSize: "0.6rem",
+                    }}
+                  >
+                    ({option.floor} floor)
+                  </Typography>
                 </Box>
-            </Box>
-        </Modal>
+              )}
+              sx={{
+                "& .MuiAutocomplete-input": {
+                  fontSize: "1.2rem", // Increase font size of input
+                },
+                "& .MuiAutocomplete-option": {
+                  fontSize: "1.2rem", // Increase font size of dropdown options
+                  padding: "10px 15px", // Add padding for elegance
+                },
+                "& .MuiOutlinedInput-root": {
+                  borderRadius: "25px", // Rounded corners for elegance
+                },
+              }}
+            />
+          </Box>
 
-      </Box>
-    </Container>
+          <Box mt={2}>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={() => setOpen(true)}
+            >
+              I'm here!
+            </Button>
+          </Box>
+
+          <Modal open={open} onClose={() => setOpen(false)}>
+            <Box
+              sx={{
+                position: "absolute",
+                top: "50%",
+                left: "50%",
+                transform: "translate(-50%, -50%)",
+                width: "95%",
+                bgcolor: "background.paper",
+                border: "2px solid #000",
+                boxShadow: 24,
+                p: 4,
+              }}
+            >
+              <Typography variant="h6" id="modal-title">
+                Calibration Required
+              </Typography>
+              <Carousel
+                showThumbs={false}
+                showStatus={false}
+                showIndicators={false}
+                selectedItem={currentSlide}
+                showArrows={false}
+              >
+                <div>
+                  <img src={goingToShop} alt="Step 1" />
+                  <Typography
+                    variant="body2"
+                    id="modal-description"
+                    gutterBottom
+                  >
+                    <b>Step 1:</b> Go to the {currentLocation?.name}.
+                  </Typography>
+                </div>
+                <div>
+                  <img src={facingToShop} alt="Step 2" />
+                  <Typography
+                    variant="body2"
+                    id="modal-description"
+                    gutterBottom
+                  >
+                    <b>Step 2:</b> Face towards the shop:{" "}
+                    {currentLocation?.name}.
+                  </Typography>
+                </div>
+              </Carousel>
+              <Box display="flex" justifyContent="space-between" mt={2}>
+                {currentSlide === 1 && (
+                  <Button variant="text" onClick={handlePrevious}>
+                    &lt; Previous
+                  </Button>
+                )}
+                <CountdownButton
+                  handlePrevious={handleNext}
+                  buttonText={currentSlide === 0 ? "Next" : "Calibrate"}
+                />
+              </Box>
+            </Box>
+          </Modal>
+        </Box>
+      </Container>
     </>
   );
 };
